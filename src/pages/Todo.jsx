59da6d90bfca3af003842891bfbcb506a0c32218
shakhawat.dev <shakhawat.code@gmail.com>
2025-04-11 10:56:03 +0600

    todo complete

--- src/pages/Todo.jsx
+++ src/pages/Todo.jsx
@@ -6,19 +6,39 @@ import { useDispatch, useSelector } from "react-redux";
 
 // icons
 import { MdOutlinePlaylistAdd } from "react-icons/md";
+import { CiEdit } from "react-icons/ci";
+import { RiDeleteBin4Line } from "react-icons/ri";
+import { MdDone } from "react-icons/md";
 
 
 // slices/reduces
 import { inputval } from "../redux/slices/todoSlice";
+import { addTask } from "../redux/slices/todoSlice";
+import { isvalid } from "../redux/slices/todoSlice";
+import { isEdit } from "../redux/slices/todoSlice";
+import { editDone } from "../redux/slices/todoSlice";
+import { deleteTask } from "../redux/slices/todoSlice";
+
 
 
 export const Todo = () => {
 
     const dispatch = useDispatch()
-    const inputValue = useSelector((state)=> state.todo.value)
+    const inputValue = useSelector((state)=> state.todo.inputVal)
+    const taskArr = useSelector((state)=> state.todo.taskArr)
+    const isValidInput = useSelector((state)=> state.todo.isValidInput )
+    const isedit = useSelector((state)=> state.todo.edit);
+ 
 
-console.log(inputValue);
-
+    const handelAdd = ()=> {
+      if(inputValue){
+        dispatch(isvalid(true))
+        dispatch(addTask())
+      }
+      else{
+        dispatch(isvalid(false))
+      }
+    }
 
 
   return (
@@ -27,18 +47,28 @@ console.log(inputValue);
 
         {/* input box */}
         <div className="flex w-full justify-center lg:w-1/4   ">
-          <input onChange={(e)=> dispatch(inputval(e.target.value))} type="text" placeholder="Type here" className="input w-full " />
-          <button className="btn border btn-outline ">
-            Add
-            <MdOutlinePlaylistAdd className="text-2xl"/>
-          </button>
+          <input value={inputValue} onChange={(e)=> dispatch(inputval(e.target.value))} type="text" placeholder="Type here" className={`input w-full      ${!isValidInput && " border border-red-500"}  `} />
+          
+          {!isedit && <button onClick={()=> handelAdd()} className="btn border btn-outline "> Add <MdOutlinePlaylistAdd className="text-2xl"/> </button>}
+          {isedit && <button onClick={()=> dispatch(editDone())} className="btn border btn-outline "> Done <MdDone className="text-2xl"/> </button>}
         </div>
 
         {/* list box */}
+        {taskArr.length > 0 ? 
         <ul className="menu menu-md bg-base-200 rounded-box flex w-full justify-center lg:w-1/4">
-          <li></li>
-          <li></li>
+          {taskArr.map((task , index)=> (
+            <li className="flex-row items-center justify-between  " key={index}>
+              <p><span>{index+1}.</span>{task}</p>
+              <div className="flex ">
+                <button onClick={()=> dispatch(isEdit(index))} ><CiEdit className="text-2xl"/></button>
+                <button onClick={()=> dispatch(deleteTask(index))}  className="ml-5 "><RiDeleteBin4Line className="text-xl"/></button>
+              </div>  
+            </li>
+            ))}
         </ul>
+        :
+        <h3 className="mt-10">There is no task you have added , Please add some tasks.</h3>
+        }
       </div>
     </div>
   );
--- src/redux/slices/todoSlice.js
+++ src/redux/slices/todoSlice.js
@@ -3,16 +3,41 @@ import { createSlice } from '@reduxjs/toolkit'
 export const todoSlice = createSlice({
   name: 'todo',
   initialState: {
-    value: 0
+    inputVal: "",
+    taskArr: [],
+    isValidInput: true,
+    edit: false,
+    taskIndex: null,
   },
   reducers: {
     inputval: (state , action)=>{
-        state.value = action.payload
+        state.inputVal = action.payload;
+        state.isValidInput = true;
+    },
+    addTask: (state , action)=>{
+      state.taskArr.push(state.inputVal);
+      state.inputVal = "";
+    },
+    isvalid: (state , action)=> {
+      state.isValidInput = action.payload
+    },
+    isEdit: (state , action)=>{
+      state.edit = true;
+      state.taskIndex = action.payload;
+      state.inputVal = state.taskArr[action.payload]
+    },
+    editDone: (state )=>{
+      state.taskArr[state.taskIndex] = state.inputVal;
+      state.inputVal = "";
+      state.edit = false;
+    },
+    deleteTask: (state , action)=>{
+      state.taskArr.splice(action.payload , 1)
     }
   }
 })
 
 // Action creators are generated for each case reducer function
-export const { inputval } = todoSlice.actions
+export const { inputval , addTask , isvalid , isEdit , editDone , deleteTask} = todoSlice.actions
 
 export default todoSlice.reducer
